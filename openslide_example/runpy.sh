6836db8d48c0b0159b68ce8d68bc2d51287a28ad
jylin <r08922170@ntu.edu.tw>
2021-02-02 23:01:54 +0800

    modify runpy.sh  read_svs.py

--- openslide_example/runpy.sh
+++ openslide_example/runpy.sh
@@ -1,4 +1,5 @@
 # python read_svs.py &
 # lsof -p $! | grep cairo  # /usr/lib/x86_64-linux-gnu/libcairo.so.2.11600.0
+# lsof -p $! | grep pixman  # /usr/lib/x86_64-linux-gnu/libpixman-1.so.0.38.4
 
-py-spy record -n -o openslide_new.svg --  python read_svs.py
+py-spy record -n -o openslide_new_cairo_pixman.svg --  python read_svs.py
